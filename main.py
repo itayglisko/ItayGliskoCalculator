655ca46ad5fcdbe5e2c70bd67c698c29437d7b4f
Your Name <OmegaCollegeUser123@gmail.com>
2023-12-17 22:10:34 +0200

    addition func

---
+++ main.py
@@ -0,0 +1,5 @@
+def add(num1,num2):
+    if type(num1) is int or type(num1) is float and type(num2) is int or type(num2) is float:
+        print(num1+num2)
+    else:
+        print("invalid input")
