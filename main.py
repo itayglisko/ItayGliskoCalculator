27497ac14cb46527437526b4481c62cf904f0d31
Your Name <OmegaCollegeUser123@gmail.com>
2023-12-17 22:15:29 +0200

    pow func

--- main.py
+++ main.py
@@ -20,3 +20,10 @@ def div(num1, num2):
         print("u cant divide with zero")
     else:
         print(num1/num2)
+def pow(num1, num2):
+    if type(num1) is not int and type(num1) is not float or type(num2) is not int and type(num2) is not float:
+        print("invalid input")
+    elif num2 == 0 and num1 ==0:
+        print("undefined")
+    else:
+        print(num1 ** num2)
