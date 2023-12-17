db34de20fd9ba11a6770772f4c505d61eab44c40
Your Name <OmegaCollegeUser123@gmail.com>
2023-12-17 22:13:00 +0200

    mul func

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ def sub(num1, num2):
         print(num1-num2)
     else:
         print("invalid input")
+def mul(num1, num2):
+    if type(num1) is int or type(num1) is float and type(num2) is int or type(num2) is float:
+        print(num1*num2)
+    else:
+        print("invalid input")
